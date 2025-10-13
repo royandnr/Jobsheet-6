bd04c6761afbb1011709914697ef5250d243e0fa
Yann <yann@gmail.com>
2025-10-13 19:55:14 +0700

    input data mahasiswa

---
+++ StatusKelulusanPerMataKuliah.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;                       // import library
+
+public class StatusKelulusanPerMataKuliah   {   // bagian class
+
+    public static void main(String[] args) {    // Method Main
+        Scanner sc = new Scanner(System.in);    // perintah atau statement
+
+        // input data mahasiswa
+        System.out.println("===== INPUT DATA MAHASISWA ====="); 
+        System.out.print("Nama : ");
+        String nama = sc.nextLine();
+        System.out.print("NIM : ");
+        String nim = sc.nextLine();
+
+        
+
+
+    }
+}
